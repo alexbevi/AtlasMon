097f720e42a1930d28eb69d6bea02b11bdec75e2
Chris Grabosky <chris@grabosky.net>
2019-09-19 12:22:19 -0400

    MVP for logging all clusters in proj and entering data

---
+++ Stitch-Export/functions/generateDailyUseLog/source.js
@@ -0,0 +1,39 @@
+exports = async function(arg){
+  // get variables
+  var conn = context.services.get("mongodb-atlas").db("atlasmonitor").collection("log");
+  var orgID = context.values.get("orgID");
+  var apiRoot = context.values.get("apiRoot");
+  var apiKey = context.values.get("atlasAPI");
+  const httpService = context.services.get("hook");
+  
+
+  // make api call
+  uri = "https://"+apiKey.key+":"+apiKey.secret+"@cloud.mongodb.com/api/atlas/v1.0/orgs/"+orgID+"/groups"
+  var args = {"url":uri, "headers": {"Content-Type":["application/json"]}, "digestAuth": true };
+  var result = httpService.get(args, {"encodeBodyAsJSON":true});
+  var res = await result;
+  
+  // build doc to be saved
+  var doc = {};
+  doc.queryTime = new Date();
+  doc.atlasOrgResponse = JSON.parse(res.body.text());
+  doc.projects = [];
+  
+    
+  for(var orgNum = 0; orgNum < doc.atlasOrgResponse.results.length; orgNum++) {
+    uri = "https://"+apiKey.key+":"+apiKey.secret+"@cloud.mongodb.com/api/atlas/v1.0/groups/"+doc.atlasOrgResponse.results[orgNum].id+"/clusters";
+    args = {"url":uri, "headers": {"Content-Type":["application/json"]}, "digestAuth": true };
+    result = httpService.get(args, {"encodeBodyAsJSON":true});
+    res = await result;
+    
+    var proj = {};
+    proj.name = doc.atlasOrgResponse.results[orgNum].name;
+    proj.id = doc.atlasOrgResponse.results[orgNum].id;
+    proj.clustersResponse = JSON.parse(res.body.text());
+    doc.projects.push(proj);
+  }
+  
+  // write to DB
+  conn.insertOne(doc);
+  
+};
