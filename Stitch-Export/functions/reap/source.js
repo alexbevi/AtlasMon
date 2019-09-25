f3873d423df52649918ed3f166dd1f37b815109c
Chris Grabosky <chris@grabosky.net>
2019-09-25 08:55:10 -0400

    we are live

--- Stitch-Export/functions/reap/source.js
+++ Stitch-Export/functions/reap/source.js
@@ -3,15 +3,22 @@ exports = function(changeEvent) {
   
   //if doc.warnings > 1) {
     // send a notification
+    // context.functions.execute("sendText", doc.owner.id, "Your cluster " + doc.name + " will be reaped soon.");
   //}
   
   if(doc.warnings > 2) {
     if(!doc.hasOwnProperty("reaped")){
       var conn = context.services.get("mongodb-atlas").db("atlasmonitor").collection("clusters");
       var n = new Date();
-      //context.functions.execute("pauseCluster", doc.projectId, doc.name);
-      //context.functions.execute("sendText", doc.owner.id, "Your cluster " + doc.name + " has been reaped (auto paused).");
-      //conn.updateOne({_id:doc._id}, {$set:{reaped:true, updated:n}});
+      context.functions.execute("pauseCluster", doc.projectId, doc.name);
+      context.functions.execute("sendText", doc.owner.id, "Your cluster " + doc.name + " has been reaped (auto paused).");
+      conn.updateOne({_id:doc._id}, {$set:{reaped:true, updated:n}});
+      if(doc.name.toLowerCase().indexOf("poc") !== -1) {
+        const validAdmins = context.values.get("validAdmins");
+        for(var i = 0; i < validAdmins.length; i++) {
+          context.functions.execute("sendText", validAdmins[i], "A cluster with the name 'POC' has been reaped: " + doc.name + " (auto paused).");
+        }
+      }
     }
   }
   
