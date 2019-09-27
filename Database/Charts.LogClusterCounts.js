534099c3c00ef860e69342e28485df66bb890df1
Chris Grabosky <chris@grabosky.net>
2019-09-27 10:44:15 -0400

    Updated graph

--- Database/Charts.LogClusterCounts.js
+++ Database/Charts.LogClusterCounts.js
@@ -1,14 +1,34 @@
-[{$project: {
-    queryTime:1,
-    clusCount:{$sum:"$projects.clustersResponse.totalCount"}
-  }}, {$group: {
-    _id:{
-      $dateToString:{
-        date:"$queryTime",
-        format:"%Y-%m-%d"
-      }
-    },
-    avgClusCount:{$avg:"$clusCount"}
-  }}, {$sort: {
-    _id: 1
-  }}]
+[{$unwind: {
+  path: "$projects"
+}}, {$project: {
+  totalCount:"$projects.clustersResponse.totalCount",
+  queryTime:1,
+  paused:{
+    $filter: {
+      input: "$projects.clustersResponse.results",
+      as:"c",
+      cond:{ $eq:["$$c.paused",true]}
+    }
+  }
+}}, {$project: {
+  queryTime: 1,
+  totalCount:1,
+  pausedCount: {$size:"$paused"}
+}}, {$group: {
+  _id:"$queryTime",
+  totalCount:{$sum:"$totalCount"},
+  pausedCount:{$sum:"$pausedCount"}
+}}, {$group: {
+  _id: {
+    $dateToString: {
+      date: '$_id',
+      format: '%Y-%m-%d'
+    }
+  },
+  avgClusCount: {
+    $avg: '$totalCount'
+  },
+  pausedCout: {
+    $avg: "$pausedCount"
+  }
+}}]
