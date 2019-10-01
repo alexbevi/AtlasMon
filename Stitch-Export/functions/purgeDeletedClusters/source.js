c8fe1b90f24dacb6c322221c4b8e00b18b896f8d
Chris Grabosky <chris@grabosky.net>
2019-10-01 15:34:57 -0400

    bug in purge

--- Stitch-Export/functions/purgeDeletedClusters/source.js
+++ Stitch-Export/functions/purgeDeletedClusters/source.js
@@ -2,13 +2,14 @@ exports = async function(){
   // get variables
   var conn = context.services.get("mongodb-atlas").db("atlasmonitor").collection("clustersNoLongerInLog");
   var log = context.services.get("mongodb-atlas").db("atlasmonitor").collection("changelog");
+  var clus = context.services.get("mongodb-atlas").db("atlasmonitor").collection("clusters");
   
   var docs = await conn.find().toArray();
     
   for(var i = 0; i < docs.length; i++) {
     var n = new Date();
     log.insertOne({clustersId:docs[i]._id,eventType:"deleteClusterFromList",fromAutomation:true,updated:n});
-    conn.deleteOne({_id:docs[i]._id});
+    clus.deleteOne({_id:docs[i]._id});
   }
 
   
