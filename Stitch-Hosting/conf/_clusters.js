8ea5e9ba4c57abc95e15a97793cbf059511f5574
Chris Grabosky <chris@grabosky.net>
2019-10-03 10:54:48 -0400

    use grid so pretty much wow

--- Stitch-Hosting/conf/_clusters.js
+++ Stitch-Hosting/conf/_clusters.js
@@ -2,7 +2,8 @@ const CHARTSURIS = [
 ];
 
 $(function() {
+    var gridSize = 12 / CHARTSURIS.length;
     $.each(CHARTSURIS, function(index, uri) {
-        $('#ctr_charts').append('<iframe style="border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);margin-top:20px;margin-bottom:20px;" width="50%" height="200" src="'+uri+'"></iframe>');
+        $('#ctr_charts').append('<div class="col-sm-'+gridSize+'"><iframe style="border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);margin-top:20px;margin-bottom:20px;" width="100%" height="200" src="'+uri+'"></iframe></div>');
     });
 });
