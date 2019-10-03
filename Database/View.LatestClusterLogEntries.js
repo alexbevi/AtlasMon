f105b5aeebda911efdac2886d778c924146077a3
Chris Grabosky <chris@grabosky.net>
2019-10-03 09:13:10 -0400

    view include pause status and more

--- Database/View.LatestClusterLogEntries.js
+++ Database/View.LatestClusterLogEntries.js
@@ -1,15 +1,40 @@
-[{$sort: {
-    queryTime: -1
-  }}, {$limit: 1}, {$unwind: {
-    path: '$projects'
-  }}, {$project: {
-    projectName: '$projects.name',
-    clusters: '$projects.clustersResponse.results.name'
-  }}, {$unwind: {
-    path: '$clusters'
-  }}, {$project: {
-    _id:0,
-    projectName:1,
-    cluster:"$clusters",
-    combinedName:{$concat:["$projectName","/","$clusters"]}
-  }}]
+[
+  {$sort: {
+  queryTime: -1
+}}, 
+{$limit: 1}, 
+{$unwind: {
+  path: '$projects'
+}}, 
+{$project: {
+  projectName: '$projects.name',
+  'projects.clustersResponse.results.name': 1,
+  'projects.clustersResponse.results.paused': 1
+}}, 
+{$project: {
+  projectName: 1,
+  clusters: '$projects.clustersResponse.results'
+}}, 
+{$unwind: {
+  path: '$clusters'
+}}, 
+{$project: {
+  _id: 0,
+  projectName: 1,
+  cluster: '$clusters.name',
+  paused: '$clusters.paused',
+  combinedName: {
+    $concat: [
+      '$projectName',
+      '/',
+      '$clusters.name'
+    ]
+  },
+  wordMap: {
+    $split: [
+      '$clusters.name',
+      '-'
+    ]
+  }
+}}
+]
