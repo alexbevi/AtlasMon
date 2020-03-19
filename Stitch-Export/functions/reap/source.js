c21d02725d3b69bbd6e351995b8ee696b6467896
Chris Grabosky <chris@grabosky.net>
2020-03-19 09:25:09 -0400

    only reap the on weekend reaps via weekend trigger, not daily trigger

--- Stitch-Export/functions/reap/source.js
+++ Stitch-Export/functions/reap/source.js
@@ -4,28 +4,31 @@ exports = function(changeEvent) {
   if(doc.changeSrc == "cron") {
     var shouldNotReap = (doc.noReap || false);
     var alreadyReaped = (doc.reaped || false);
+    var pauseWeekendsOnly = (doc.pauseWeekends || false)
     
-    if(doc.warnings > 2) {
-      if(!alreadyReaped && !shouldNotReap){
-        if((doc.size!="M0") && (doc.size!="M2") && (doc.size!="M5")) {
-          console.log("Reaping... " + doc._id);
-          var conn = context.services.get("mongodb-atlas").db("atlasmonitor").collection("clusters");
-          var n = new Date();
-          context.functions.execute("pauseCluster", doc.projectId, doc.name);
-          context.functions.execute("sendText", doc.owner.id, "Your cluster " + doc.name + " has been reaped (auto paused).");
-          conn.updateOne({_id:doc._id}, {$set:{reaped:true, updated:n}});
-          if(doc.name.toLowerCase().indexOf("poc") !== -1) {
-            const validAdmins = context.values.get("validAdmins");
-            for(var i = 0; i < validAdmins.length; i++) {
-              context.functions.execute("sendText", validAdmins[i], "A cluster with the name 'POC' has been reaped: " + doc.name + " (auto paused).");
+    if(!pauseWeekendsOnly) {
+      if(doc.warnings > 2) {
+        if(!alreadyReaped && !shouldNotReap){
+          if((doc.size!="M0") && (doc.size!="M2") && (doc.size!="M5")) {
+            console.log("Reaping... " + doc._id);
+            var conn = context.services.get("mongodb-atlas").db("atlasmonitor").collection("clusters");
+            var n = new Date();
+            context.functions.execute("pauseCluster", doc.projectId, doc.name);
+            context.functions.execute("sendText", doc.owner.id, "Your cluster " + doc.name + " has been reaped (auto paused).");
+            conn.updateOne({_id:doc._id}, {$set:{reaped:true, updated:n}});
+            if(doc.name.toLowerCase().indexOf("poc") !== -1) {
+              const validAdmins = context.values.get("validAdmins");
+              for(var i = 0; i < validAdmins.length; i++) {
+                context.functions.execute("sendText", validAdmins[i], "A cluster with the name 'POC' has been reaped: " + doc.name + " (auto paused).");
+              }
             }
           }
+          else {
+            console.log("No reaping free tiers... " + doc._id);
+          }
+        } else { 
+          console.log("No Reaping... " + doc._id);
         }
-        else {
-          console.log("No reaping free tiers... " + doc._id);
-        }
-      } else { 
-        console.log("No Reaping... " + doc._id);
       }
     }
   }
