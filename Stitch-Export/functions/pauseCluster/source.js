3b9058e6e4d30aab18136404da688b81adf11ae0
Chris Grabosky <chris@grabosky.net>
2020-03-19 11:08:51 -0400

    protected flag

--- Stitch-Export/functions/pauseCluster/source.js
+++ Stitch-Export/functions/pauseCluster/source.js
@@ -3,31 +3,37 @@ exports = async function(projectId, clusterName){
   if((projectId!= "5b6c7a1496e82139bc824e8b") && (clusterName != "Example")) {
     var log = context.services.get("mongodb-atlas").db("atlasmonitor").collection("changelog");
     var conn = context.services.get("mongodb-atlas").db("atlasmonitor").collection("clusters");
-    
-    var orgID = context.values.get("orgID");
-    var apiRoot = context.values.get("apiRoot");
-    var apiKey = context.values.get("atlasAPI");
-    const httpService = context.services.get("hook");
-    
-  
-    // make api call
-    uri = "https://"+apiKey.key+":"+apiKey.secret+"@cloud.mongodb.com/api/atlas/v1.0/groups/"+projectId+"/clusters/"+clusterName;
-    var data = {paused:true};
-    var args = {"url":uri, "headers": {"Content-Type":["application/json"]}, "digestAuth": true, "body":data, "encodeBodyAsJSON":true  };
-    var result = httpService.patch(args, {"encodeBodyAsJSON":true});
-    var res = await result;
-    
-    // build doc to be saved
     var doc = {};
-    var n = new Date();
-    doc.queryTime = n;
-    doc.eventType = "pause";
-    doc.atlasResponse = JSON.parse(res.body.text());
-    log.insertOne(doc);
-    conn.updateOne({"projectId":projectId, name:clusterName}, {$inc:{reapCount:1}}, {$set:{updated:n, reaped:true, noReap:false}})
     
-    console.log(doc);
+    conn.findOne({"name":clusterName, "projectId":projectId}).then((srcdoc) => {
+      if (srcdoc) {
+        var protect = (srcdoc.protected || false);
+        if(!protect) {
+          var orgID = context.values.get("orgID");
+          var apiRoot = context.values.get("apiRoot");
+          var apiKey = context.values.get("atlasAPI");
+          const httpService = context.services.get("hook");
+          
+          // make api call
+          uri = "https://"+apiKey.key+":"+apiKey.secret+"@cloud.mongodb.com/api/atlas/v1.0/groups/"+projectId+"/clusters/"+clusterName;
+          var data = {paused:true};
+          var args = {"url":uri, "headers": {"Content-Type":["application/json"]}, "digestAuth": true, "body":data, "encodeBodyAsJSON":true  };
+          var result = httpService.patch(args, {"encodeBodyAsJSON":true}).then((res) => {
+            // build doc to be saved
+            var n = new Date();
+            doc.queryTime = n;
+            doc.eventType = "pause";
+            doc.atlasResponse = JSON.parse(res.body.text());
+            log.insertOne(doc);
+            conn.updateOne({"projectId":projectId, name:clusterName}, {$inc:{reapCount:1}}, {$set:{updated:n, reaped:true, noReap:false}});
+          });
+        } else {
+          console.log("NEVER REAP " + clusterName);
+        }
+      } 
+    });
     
-    return doc;
+  console.log(doc);
+  return doc;
   }
 };
